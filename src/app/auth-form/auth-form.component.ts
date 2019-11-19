1d9bff99d1821cf053f7bb675e91d66c3f5aa0ae
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-11-18 22:22:47 -0500

    using ng-content with projection slots and update form template and styles

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { User } from './auth-form';
 
 @Component({
@@ -6,37 +6,28 @@ import { User } from './auth-form';
   template: /*html*/`
     <div>
       <form class="d-flex flex-column justify-content-center" #form="ngForm" no-validate (ngSubmit)="onSubmit(form.value)">
-        <ng-content></ng-content>
+        <ng-content select=h3></ng-content>
         <label>
           Email address
-          <input type=email name=email [attr.id]=emailId>
+          <input ngModel type="email" name="email">
         </label>
         <label>
           Password
-          <input type=password name=password [attr.id]=passwordId>
+          <input ngModel type="password" name="password">
         </label>
-        <button type=submit>
-          Submit
-        </button>
+        <ng-content select=button></ng-content>
       </form>
       {{form.value | json}}
     </div>
   `,
   styles: []
 })
-export class AuthFormComponent implements OnInit {
-  static count = 0;
-  current = AuthFormComponent.count++;
-  emailId = 'email-' + this.current;
-  passwordId = 'password-' + this.current;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class AuthFormComponent {
+  @Output()
+  submitted: EventEmitter<User> = new EventEmitter<User>();
 
   onSubmit(user: User) {
-    console.log(user);
+    this.submitted.emit(user);
   }
 
 }
