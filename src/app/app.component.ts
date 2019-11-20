32ef5997d2e417892e22124f1bdba9dcbacf11de
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-11-19 23:52:52 -0500

    dynamic ng-template rendering with ViewContainerRef

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewContainerRef, ViewChild, AfterContentInit, ComponentFactoryResolver, ComponentRef } from '@angular/core';
+import { Component, ViewContainerRef, ViewChild, AfterContentInit, ComponentFactoryResolver, ComponentRef, TemplateRef } from '@angular/core';
 
 import { AuthFormComponent } from './auth-form/auth-form.component';
 
@@ -8,16 +8,17 @@ import { User } from './auth-form/auth-form';
   selector: 'app-root',
   template: /* html */`
     <div>
-      <button (click)="destroyComponent()">Destroy</button>
-      <button (click)="moveComponent()">Move</button>
-      <auth-form (submitted)="$event"></auth-form>
       <div #entry></div>
+      <ng-template #tmpl>
+      Todd Motto: England UK
+      </ng-template>
     </div>
   `
 })
 export class AppComponent implements AfterContentInit {
 
   @ViewChild('entry', { read: ViewContainerRef, static: true }) entry: ViewContainerRef;
+  @ViewChild('tmpl', { static: true }) tmpl: TemplateRef<any>;
 
   constructor(
     private resolver: ComponentFactoryResolver
@@ -26,13 +27,15 @@ export class AppComponent implements AfterContentInit {
   component: ComponentRef<AuthFormComponent>;
 
   ngAfterContentInit() {
-    const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
-    this.entry.createComponent(authFormFactory);
-    this.component = this.entry.createComponent(authFormFactory, 0);
-    console.log(this.component, this.component.instance);
-    this.component.instance.title = 'Create Account';
-    this.component.instance.submitted.subscribe(this.loginUser);
-    this.entry.createComponent(authFormFactory);
+    // const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
+    // this.entry.createComponent(authFormFactory);
+    // this.component = this.entry.createComponent(authFormFactory, 0);
+    // console.log(this.component, this.component.instance);
+    // this.component.instance.title = 'Create Account';
+    // this.component.instance.submitted.subscribe(this.loginUser);
+    // this.entry.createComponent(authFormFactory);
+
+    this.entry.createEmbeddedView(this.tmpl);
   }
 
   loginUser(user: User) {
