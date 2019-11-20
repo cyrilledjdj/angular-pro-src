d520773c33737d35faf66655ed54278e1d882ac8
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-11-19 23:35:47 -0500

    destroying dynamic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { User } from './auth-form/auth-form';
   selector: 'app-root',
   template: /* html */`
     <div>
+      <button (click)="destroyComponent()">Destroy</button>
       <auth-form (submitted)="$event"></auth-form>
       <div #entry></div>
     </div>
@@ -21,15 +22,25 @@ export class AppComponent implements AfterContentInit {
     private resolver: ComponentFactoryResolver
   ) { }
 
+  component: ComponentRef<AuthFormComponent>;
+
   ngAfterContentInit() {
     const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
-    const component: ComponentRef<AuthFormComponent> = this.entry.createComponent(authFormFactory);
-    console.log(component, component.instance);
-    component.instance.title = 'Create Account';
-    component.instance.submitted.subscribe(this.loginUser);
+    this.component = this.entry.createComponent(authFormFactory);
+    console.log(this.component, this.component.instance);
+    this.component.instance.title = 'Create Account';
+    this.component.instance.submitted.subscribe(this.loginUser);
   }
 
   loginUser(user: User) {
     console.log('Login', user);
+  }
+
+  destroyComponent() {
+    this.component.destroy();
+    console.log('destroyed');
+    while (this.entry.length) {
+      this.entry.get(0).destroy();
+    }
   }
 }
