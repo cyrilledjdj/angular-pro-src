f1df3e24008a7ce8dbe0de2ab95b7f8992871aa2
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-12-08 16:28:18 -0500

    Zones and NgZones

--- projects/zone-webapp/src/app/app.component.ts
+++ projects/zone-webapp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck, NgZone } from '@angular/core';
 import { FoodStoreService } from './food-store/food-store.service';
 
 @Component({
@@ -6,8 +6,25 @@ import { FoodStoreService } from './food-store/food-store.service';
 	templateUrl: './app.component.html',
 	styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck {
 	title = 'zone-webapp';
 	store$ = this.foodService.getStore();
-	constructor(private foodService: FoodStoreService) {}
+	constructor(private foodService: FoodStoreService, private zone: NgZone) {}
+
+	counter = 0;
+
+	ngOnInit() {
+		this.zone.runOutsideAngular(() => {
+			for (let i = 0; i < 100; i++) {
+				setTimeout(() => this.counter++, 1000);
+			}
+			this.zone.run(() => {
+				setTimeout(() => (this.counter = this.counter), 3000);
+			});
+		});
+	}
+
+	ngDoCheck() {
+		console.log('change detection has been run!');
+	}
 }
