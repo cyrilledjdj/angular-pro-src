30fd214588b39ee75a17c465c5d0cb9eed34eaea
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-12-08 14:17:01 -0500

    reformat files

--- projects/zone-webapp/src/app/containers/pizza-viewer/pizza-viewer.component.ts
+++ projects/zone-webapp/src/app/containers/pizza-viewer/pizza-viewer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+
 import { FoodService } from '../../food.service';
+
 export interface Pizza {
 	name: string;
 	price: number;
--- projects/zone-webapp/src/app/containers/side-viewer/side-viewer.component.ts
+++ projects/zone-webapp/src/app/containers/side-viewer/side-viewer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+
 import { FoodService } from '../../food.service';
 
 export interface Side {
