b0b2598e93617237d3981ecdad459bb3f58ca8a2
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-12-07 23:54:04 -0500

    testing component templates

--- src/app/stock-inventory/components/stock-counter/stock-counter.component.spec.ts
+++ src/app/stock-inventory/components/stock-counter/stock-counter.component.spec.ts
@@ -1,10 +1,13 @@
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StockCounterComponent } from './stock-counter.component';
+import { By } from '@angular/platform-browser';
 
 describe('StockCounterComponent', () => {
 	let component: StockCounterComponent;
 	let fixture: ComponentFixture<StockCounterComponent>;
+	let el: DebugElement;
 
 	beforeEach(
 		async(() => {
@@ -17,6 +20,7 @@ describe('StockCounterComponent', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(StockCounterComponent);
 		component = fixture.componentInstance;
+		el = fixture.debugElement;
 		component.value = 0;
 		component.step = 1;
 		component.min = 0;
@@ -59,4 +63,36 @@ describe('StockCounterComponent', () => {
 
 	// Changes to use ControlValueAccessor no longer need the @Output
 	xit('should call the output on a value change', () => {});
+
+	it('should increment when the + button is clicked', () => {
+		el.query(By.css('button:first-child')).triggerEventHandler('click', null);
+		fixture.detectChanges();
+		expect(component.value).toBe(1);
+		expect(el.query(By.css('p')).nativeElement.textContent).toBe('1');
+	});
+
+	it('should increment the value when the up arrow is pressed', () => {
+		const event = new Event('KeyboardEvent') as any;
+		event.code = 'ArrowUp';
+		el.query(By.css('.stock-counter > div > div')).triggerEventHandler('keydown', event);
+		fixture.detectChanges();
+		expect(component.value).toBe(1);
+	});
+
+	it('should decrement when the - button is clicked', () => {
+		component.value = 1;
+		el.query(By.css('button:last-child')).triggerEventHandler('click', null);
+		fixture.detectChanges();
+		expect(component.value).toBe(0);
+		expect(el.query(By.css('p')).nativeElement.textContent).toBe('0');
+	});
+
+	it('should decrement the value when the down arrow is pressed', () => {
+		component.value = 1;
+		const event = new Event('KeyboardEvent') as any;
+		event.code = 'ArrowDown';
+		el.query(By.css('.stock-counter > div > div')).triggerEventHandler('keydown', event);
+		fixture.detectChanges();
+		expect(component.value).toBe(0);
+	});
 });
