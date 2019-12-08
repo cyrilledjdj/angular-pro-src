cf5276e851df8de71af887224ca993015a8b766c
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-12-07 20:46:52 -0500

    testing isolate pipes

--- projects/karma-webapp/src/app/app.module.ts
+++ projects/karma-webapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FileSizePipe } from './file-size/file-size.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileSizePipe
   ],
   imports: [
     BrowserModule,
---
+++ projects/karma-webapp/src/app/file-size/file-size.pipe.spec.ts
@@ -0,0 +1,33 @@
+import { FileSizePipe } from './file-size.pipe';
+
+describe('FileSizePipe', () => {
+
+  describe('Isolate FileSizePipe test: ', () => {
+    let pipe;
+    beforeEach(() => {
+      pipe = new FileSizePipe();
+    });
+
+    it('create an instance', () => {
+      expect(pipe).toBeTruthy();
+    });
+
+    it('should convert bytes to megabytes', () => {
+      expect(pipe.transform(123456789)).toBe('117.74MB');
+      expect(pipe.transform(987654321)).toBe('941.90MB');
+    });
+
+    it('should use the default extension when not supplied', () => {
+
+      expect(pipe.transform(123456789)).toBe('117.74MB');
+      expect(pipe.transform(987654321)).toBe('941.90MB');
+    });
+
+    it('should override the extension when supplied', () => {
+
+      expect(pipe.transform(123456789, 'myExt')).toBe('117.74myExt');
+      expect(pipe.transform(987654321, 'anotherExt')).toBe('941.90anotherExt');
+    });
+  });
+
+});
