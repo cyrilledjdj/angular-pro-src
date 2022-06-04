a42611443716abbed283513941f3d933847dff53
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2022-06-04 19:45:56 -0400

    upgrade angular core

--- projects/zone-webapp/src/app/food-store/food-store.module.ts
+++ projects/zone-webapp/src/app/food-store/food-store.module.ts
@@ -5,23 +5,23 @@ import { FoodStoreService } from './food-store.service';
 import { FOOD_STORE_CONFIG, FoodStoreConfig } from './config';
 import { HttpClientModule } from '@angular/common/http';
 
-export const FOOD_PROVIDERS: Provider[] = [ FoodStoreService ];
+export const FOOD_PROVIDERS: Provider[] = [FoodStoreService];
 
 @NgModule({
-	declarations: [],
-	imports: [ CommonModule, HttpClientModule ]
+  declarations: [],
+  imports: [CommonModule, HttpClientModule],
 })
 export class FoodStoreModule {
-	static forRoot(config: FoodStoreConfig): ModuleWithProviders {
-		return {
-			ngModule: FoodStoreModule,
-			providers: [
-				FOOD_PROVIDERS,
-				{
-					provide: FOOD_STORE_CONFIG,
-					useValue: config
-				}
-			]
-		};
-	}
+  static forRoot(config: FoodStoreConfig): ModuleWithProviders<any> {
+    return {
+      ngModule: FoodStoreModule,
+      providers: [
+        FOOD_PROVIDERS,
+        {
+          provide: FOOD_STORE_CONFIG,
+          useValue: config,
+        },
+      ],
+    };
+  }
 }
